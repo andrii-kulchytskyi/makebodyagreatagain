ef48648ac67141dba24fdb0f9612304169998353
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2023-01-12 18:41:21 +0400

    1

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,189 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 1. Simple object
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+let man = {
+    name: 'John',
+    age: 28
+};
+
+let manFullCopy = {...man}
+
+
+// 2. Array of primitives
+let numbers = [1, 2, 3];
+
+let numbersFullCopy = [...numbers]
+
+
+// 3. Object inside an object
+let man1 = {
+    name: 'John',
+    age: 28,
+    mother: {
+        name: 'Kate',
+        age: 50
+    }
+};
+
+let man1FullCopy = {...man1, mother: man1.mother}
+
+
+// 4. Array of primitives inside an object
+let man2 = {
+    name: 'John',
+    age: 28,
+    friends: ["Peter", "Steven", "William"]
+};
+
+let man2FullCopy = {...man2, friends: man2.friends}
+man2FullCopy.friends[0] = "Hui"
+
+
+// 5 Array of objects
+let people = [
+    {name: "Peter", age: 30},
+    {name: "Steven", age: 32},
+    {name: "William", age: 28}
+];
+
+
+let peopleFullCopy = [...people]
+
+
+// 6 Array of objects inside object
+let man3 = {
+    name: 'John',
+    age: 28,
+    friends: [
+        {name: "Peter", age: 30},
+        {name: "Steven", age: 32},
+        {name: "William", age: 28}
+    ]
+};
+
+let man3FullCopy = {...man3, friends: man3.friends}
+
+
+// 7 Object inside an object, inside an object
+let man4 = {
+    name: 'John',
+    age: 28,
+    mother: {
+        name: "Kate",
+        age: 50,
+        work: {
+            position: "doctor",
+            experience: 15
+        }
+    }
+};
+
+let man4FullCopy = {...man4, mother: man4.mother}//  your code
+
+// 8 Array of objects inside object -> object
+let man5 = {
+    name: 'John',
+    age: 28,
+    mother: {
+        name: "Kate",
+        age: 50,
+        work: {
+            position: "doctor",
+            experience: 15
+        },
+        parents: [
+            {name: "Kevin", age: 80},
+            {name: "Jennifer", age: 78},
+        ]
+    }
+};
+
+let man5FullCopy = {...man5}
+
+
+// 9 Object inside an object -> array -> object ->  object
+let man6 = {
+    name: 'John',
+    age: 28,
+    mother: {
+        name: "Kate",
+        age: 50,
+        work: {
+            position: "doctor",
+            experience: 15
+        },
+        parents: [
+            {
+                name: "Kevin",
+                age: 80,
+                favoriteDish: {
+                    title: "borscht"
+                }
+            },
+            {
+                name: "Jennifer",
+                age: 78,
+                favoriteDish: {
+                    title: "sushi"
+                }
+            },
+        ]
+    }
+};
+
+let man6FullCopy  //  your code
+
+
+//10 Array of objects inside an object -> object -> array -> object ->  object
+let man7 = {
+    name: 'John',
+    age: 28,
+    mother: {
+        name: "Kate",
+        age: 50,
+        work: {
+            position: "doctor",
+            experience: 15
+        },
+        parents: [
+            {
+                name: "Kevin",
+                age: 80,
+                favoriteDish: {
+                    title: "borscht",
+                    ingredients: [
+                        {title: "beet", amount: 3},
+                        {title: "potatoes", amount: 5},
+                        {title: "carrot", amount: 1},
+                    ]
+                }
+            },
+            {
+                name: "Jennifer",
+                age: 78,
+                favoriteDish: {
+                    title: "sushi",
+                    ingredients: [
+                        {title: "fish", amount: 1},
+                        {title: "rise", amount: 0.5}
+                    ]
+                }
+            },
+        ]
+    }
+};
+
+let man7FullCopy  //  your code
+
+
+const X = () => {
+    console.log(man4)
+    console.log(man4FullCopy)
+    console.log(man5)
+    console.log(man5FullCopy)
+
+
+}
+X()
