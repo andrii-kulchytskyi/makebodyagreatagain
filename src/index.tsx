84b2899962413f2388b1bb924dd493996868a420
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2023-04-09 23:29:49 +0400

    1

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,59 @@
-type StateType = {
-    volume: number // in percents
-    trackUrl: string // 'https://blabla.com/track01.mp3',
-    currentPlayPosition: number // milliseconds,
+import React, {useMemo, useState} from 'react'
+import ReactDOM from 'react-dom'
+
+type ButtonType = {
+    id: number
+    title: string
+    forAdminOnly: boolean
+}
+const buttons: ButtonType[] = [
+    {id: 1, title: 'delete', forAdminOnly: true},
+    {id: 2, title: 'update', forAdminOnly: true},
+    {id: 3, title: 'create', forAdminOnly: false},
+]
+
+export const App = ({isAdmin}: { isAdmin: boolean }) => {
+
+    const [seconds, setSeconds] = useState(0)
+
+    const increaseSeconds = () => setSeconds(seconds + 10)
+
+    const correctButtons = useMemo(() => {
+        return buttons.filter(b => isAdmin ? true : !b.forAdminOnly)
+    }, [isAdmin])
+
+    return <>
+        <ButtonsPanel buttons={correctButtons}/>
+        <div>
+            <p>
+                <b>Секунды: {seconds}</b>
+            </p>
+            <button onClick={increaseSeconds}>
+                Увеличить на 10 секунд
+            </button>
+        </div>
+    </>
 }
 
-export const reducer = (state: StateType, action: any) => {
-    switch (action.type) {
-        case 'TRACK-URL-CHANGED':
-            return {
-                ...state,
-                trackUrl: action.url
-            }
-        case 'TRACK-MUTED':
-            return {
-                ...state,
-                volume: 0
-            }
-        case 'TRACK-REWOUND-TO-START':
-            return {
-                ...state,
-                currentPlayPosition: 0
-            }
-        default:
-            return state
-    }
-}
+const ButtonsPanel = React.memo((props: { buttons: Array<ButtonType> }) => {
+    console.log('Render ButtonsPanel')
+    return (
+        <div style={{marginBottom: '15px'}}>
+            <div style={{marginBottom: '15px'}}>
+                <b>Панель с кнопками</b>
+            </div>
+            <div>
+                {props.buttons.map(b => <button key={b.id}>{b.title}</button>)}
+            </div>
+        </div>
+    )
+})
 
-const muteTrackAC = () => ({type: 'TRACK-MUTED'})
-const changeTrackAC = (url: string) => ({type: 'TRACK-URL-CHANGED', url})
-// перемотатьНаНачало:
-const rewindToStart = () => ({type: 'TRACK-REWOUND-TO-START'})
+ReactDOM.render(<App isAdmin={true}/>, document.getElementById('root'))
 
-// Какие типы должны быть вместо XXX, YYY и ZZZ?
-// Ответ дать через пробел, например:   'BLABLA' 'HEYНЕY' 'HIPHOP'
+// Что нужно написать вместо XXX и YYY,
+// чтобы избавиться от лишнего перерендера компонента ButtonsPanel
+// при нажатии на кнопку "Увеличить на 10 секунд" ?
+
+// Ответ дайте через пробел: 111 222
+
